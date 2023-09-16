3cf78a3f43f3923dd02aa48ca22cd771bf4880ef
tohaddid <didkovskiy.anton@gmail.com>
2023-09-16 23:15:13 +0300

    .

---
+++ js/scroll-up.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  alert("!!!!!!!!!!!!!!!!");
+  function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+  }
+
+  const scrollToTopBtn = document.getElementById("scroll-btn");
+
+  function updateScrollButtonVisibility() {
+    if (window.innerWidth <= 768) {
+      scrollToTopBtn.style.display = "none";
+      return;
+    }
+
+    if (window.scrollY > 700) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  }
+
+  window.addEventListener("scroll", () => {
+    updateScrollButtonVisibility();
+  });
+
+  window.addEventListener("resize", () => {
+    updateScrollButtonVisibility();
+  });
+
+  scrollToTopBtn.addEventListener("click", () => {
+    scrollToTop();
+  });
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  updateScrollButtonVisibility();
+});
