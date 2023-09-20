f1ce01af79f0f88eb3c2d3c91beeacb34f05cd3d
tohaddid <didkovskiy.anton@gmail.com>
2023-09-20 17:11:47 +0300

    replace folder

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,23 +1,3 @@
-// Messi Goals https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
-return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
-
-// Make negative https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
-function makeNegative(num) {
-  return -Math.abs(num);
-}
-
-// Game Move https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
-return position + 2 * roll;
-
-// Personalized Message https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
-return name === owner ? "Hello boss" : "Hello guest";
-
-// Keep Hydrated https://www.codewars.com/kata/keep-hydrated-1/train/javascript
-return Math.floor(time * 0.5);
-
-// Opposites Attract https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
-return flower1 % 2 !== flower2 % 2;
-// ---------------------------------------------------------------------------------------------
 // https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 function stringToArray(string) {
   return string.split(" ");
