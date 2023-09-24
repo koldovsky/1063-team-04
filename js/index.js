b73f540b1af59c785953c57af8c50b248020df38
Serhii-Karpeniuk <142886841+Serhii-Karpeniuk@users.noreply.github.com>
2023-09-24 22:28:02 +0000

    Add js in carousel

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ document.addEventListener("partialsLoaded", () => {
   import("./trending-header-text.js");
   import("./header-clock.js");
   import("./product-list.js");
-  import("./products-carousel.js");
+  import("./trending-products-carousel.js");
+  import("./trending-carousel.js");
   import("./scroll-up.js");
   import("./shopping-cart.js");
   import('./new-arrivals.js');
