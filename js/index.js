2d08e75c97603aea626dff786a7811c064ef3228
SvyatoslavDuma <45374718+SvyatoslavDuma@users.noreply.github.com>
2023-09-24 21:25:02 +0000

    change index.js

--- js/index.js
+++ js/index.js
@@ -5,10 +5,7 @@ document.addEventListener("partialsLoaded", () => {
   import("./products-carousel.js");
   import("./scroll-up.js");
   import("./shopping-cart.js");
-<<<<<<< HEAD
   import("./testimonials.js");
-=======
   import('./new-arrivals.js');
   import('./new-arrivals-carousel-cards.js');
->>>>>>> 463e5621acdbfaea36cce6649ea239ee64c8991c
 });
