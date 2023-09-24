6fecd20110a3e622a6b3e2dcaa956de7b3defd41
Serhii-Karpeniuk <142886841+Serhii-Karpeniuk@users.noreply.github.com>
2023-09-24 15:53:20 +0000

    Upgrade css

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('partialsLoaded', () => {
+    import('./trending-header-text.js');
+    import('./header-clock.js');
+    import('./product-list.js');
+    import('./products-carousel.js');
+});
