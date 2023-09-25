66d78f04a120032082ad01bfe88849efb462137c
SofiiaYevush <142519729+SofiiaYevush@users.noreply.github.com>
2023-09-25 06:51:07 +0000

    Add js to About us

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ document.addEventListener("partialsLoaded", () => {
   import("./trending-header-text.js");
   import("./header-clock.js");
   import("./product-list.js");
+  import("./about-us.js");
   import("./products-carousel.js");
   import("./scroll-up.js");
   import("./shopping-cart.js");
