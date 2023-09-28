3aa71b885f0eeb19b9da5d9056b33011170d0e94
tohaddid <didkovskiy.anton@gmail.com>
2023-09-28 15:42:55 +0300

    change more js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-document.addEventListener("partialsLoaded", () => {
-  import("./trending-header-text.js");
-  import("./scroll-up.js");
-  import("./shopping-cart.js");
-  import("./testimonials.js");
-  import("./new-arrivals.js");
-  import("./new-arrivals-carousel-cards.js");
-  import("./trending-products-carousel.js");
-  import("./about-us.js");
-  import("./carousel-any-products.js");
-  import("./recommend-tab.js");
+document.addEventListener("partialsLoaded", async () => {
+  await import("./trending-header-text.js");
+  await import("./scroll-up.js");
+  await import("./shopping-cart.js");
+  await import("./recommend-list.js");
+  await import("./testimonials.js");
+  await import("./new-arrivals.js");
+  await import("./new-arrivals-carousel-cards.js");
+  await import("./trending-products-carousel.js");
+  await import("./about-us.js");
+  await import("./carousel-any-products.js");
 });
