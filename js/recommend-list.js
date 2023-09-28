6bfefa68c48917b94d5d1a6d33714f87a0169c66
tohaddid <didkovskiy.anton@gmail.com>
2023-09-28 22:08:48 +0300

    make recommend list

--- js/recommend-list.js
+++ js/recommend-list.js
@@ -1,56 +1,63 @@
-const responseRecommendList = await fetch("api/recommend-list.json");
-const recommendList = await responseRecommendList.json();
+const arrayAnimalCategory = [
+  "dogs",
+  "cats",
+  "parrots",
+  "rabbits",
+  "hamsters",
+  "fish",
+];
 
-function renderRecommendProducts(recommendList) {
+let activeButtonIndex = 0;
+let activeRecommendButton = document.querySelector(
+  ".recommend__cards-tab-btn.active"
+);
+const recommendList = await (await fetch("api/recommend-list.json")).json();
+
+function renderRecommendProducts() {
   const partialFirst = document.querySelector(".recommend-partial-first");
-  let currentCountProduct = 0;
   const partialSecond = document.querySelector(".recommend-partial-second");
+
   partialFirst.innerHTML = "";
   partialSecond.innerHTML = "";
+
+  const activeCategory = arrayAnimalCategory[activeButtonIndex];
+
+  let currentCountProduct = 0;
+
   for (const product of recommendList) {
-    if (product.id === "dogs") {
-      if (currentCountProduct < 2) {
-        partialFirst.innerHTML += `
-			<article class="recommend-card">
-			<div class="recommend-card__image">
-			<a href="#">
-				<img src="${product.image}" alt="${product.title}">
-			</a>
-			</div>
-			<a href="#" class="recommend-card__title">
-			${product.title}
-			</a>
-			<p class="recommend-card__price">${product.price}</p>
-			<button class="recommend-card__button" type="button">Add to Cart</button>
-			</article>`;
-        currentCountProduct++;
-      } else {
-        partialSecond.innerHTML += `
-				<article class="recommend-card">
-				<div class="recommend-card__image">
-					<a href="#">
-						<img src="${product.image}" alt="${product.title}">
-					</a>
-				</div>
-				<a href="#" class="recommend-card__title">
-					${product.title}
-				</a>
-				<p class="recommend-card__price">${product.price}</p>
-				<button class="recommend-card__button" type="button">Add to Cart</button>
-			</article>
-			`;
-      }
+    if (product.id === activeCategory) {
+      const partial = currentCountProduct < 2 ? partialFirst : partialSecond;
+
+      partial.innerHTML += `
+        <article class="recommend-card">
+          <div class="recommend-card__image">
+            <a href="#">
+              <img src="${product.image}" alt="${product.title}">
+            </a>
+          </div>
+          <a href="#" class="recommend-card__title">
+            ${product.title}
+          </a>
+          <p class="recommend-card__price">$${product.price.toFixed(2)}</p>
+          <button class="recommend-card__button" type="button">Add to Cart</button>
+        </article>`;
+
+      currentCountProduct++;
     }
   }
+
+  console.log(activeButtonIndex);
 }
+renderRecommendProducts();
 
-renderRecommendProducts(recommendList);
+const recommendTabBtn = document.querySelectorAll(".recommend__cards-tab-btn");
 
-const buttons = document.querySelectorAll(".recommend__cards-tab-btn");
-
-buttons.forEach((button) => {
+recommendTabBtn.forEach((button, index) => {
   button.addEventListener("click", () => {
-    buttons.forEach((btn) => btn.classList.remove("active"));
+    recommendTabBtn.forEach((btn) => btn.classList.remove("active"));
     button.classList.add("active");
+    activeButtonIndex = index;
+
+    renderRecommendProducts();
   });
 });
