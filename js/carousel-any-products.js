a0607d3950fff02fc9a59fc847d0201689e85190
Liza-zoz <zozylyaliza@gmail.com>
2023-09-30 14:32:49 +0300

    change js

--- js/carousel-any-products.js
+++ js/carousel-any-products.js
@@ -7,39 +7,43 @@ const prevButton = document.querySelector(".carousel__button-prev-product");
 const nextButton = document.querySelector(".carousel__button-next-product");
 
 prevButton.addEventListener("click", () => {
-  currentPosition = (currentPosition - 1 + totalContainers) % totalContainers;
-  updateCarousel();
+    currentPosition = (currentPosition - 1 + totalContainers) % totalContainers;
+    updateCarousel();
 });
 
 nextButton.addEventListener("click", () => {
-  currentPosition = (currentPosition + 1) % totalContainers;
-  updateCarousel();
+    currentPosition = (currentPosition + 1) % totalContainers;
+    updateCarousel();
 });
 
 function updateCarousel() {
-  caroselContainers.forEach((container, index) => {
-    const newIndex = (index + currentPosition) % totalContainers;
-    if (index >= currentPosition && index < currentPosition + visibleContainers) {
-      container.style.display = "block";
-    } else {
-      container.style.display = "none";
+    caroselContainers.forEach((container, index) => {
+        const newIndex = (index + currentPosition) % totalContainers;
+        if (index >= currentPosition && index < currentPosition + visibleContainers) {
+            container.style.display = "block";
+        } else {
+            container.style.display = "none";
+        }
+    });
+
+    if (currentPosition + visibleContainers > totalContainers) {
+        const remainingCards = (currentPosition + visibleContainers) - totalContainers;
+        for (let i = 0; i < remainingCards; i++) {
+            caroselContainers[i].style.display = "block";
+        }
     }
-  });
 
-  if (currentPosition + visibleContainers > totalContainers) {
-    caroselContainers[0].style.display = "block";
-  }
-
-  carousel.style.transition = "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
-  carousel.style.opacity = 0;
-  carousel.style.transform = "translateY(100%)"; 
-  setTimeout(() => {
-    carousel.style.opacity = 1;
-    carousel.style.transform = "translateY(0)";
-  }, 0); 
+    carousel.style.transition = "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
+    carousel.style.opacity = 0;
+    carousel.style.transform = "translateY(100%)";
+    setTimeout(() => {
+        carousel.style.opacity = 1;
+        carousel.style.transform = "translateY(0)";
+    }, 0);
 }
 
 updateCarousel();
+
 
 
 
