225d5015e803d9dd35378ea2769fb31189616af7
Liza-zoz <zozylyaliza@gmail.com>
2023-09-30 13:39:51 +0300

    chsnge js carousel-any-product

--- js/carousel-any-products.js
+++ js/carousel-any-products.js
@@ -1,42 +1,39 @@
-const carousel = document.querySelector(".carousel-container-product-card");
-const caroselContainers = document.querySelectorAll(".carosel-conteiner-product");
-const totalContainers = caroselContainers.length;
-const visibleContainers = 3;
-let currentPosition = 0;
-const prevButton = document.querySelector(".prev-button");
-const nextButton = document.querySelector(".next-button");
+const carouselContainer = document.querySelector(".carousel-container-product-card");
+const carouselItems = document.querySelectorAll(".carosel-conteiner-product");
+const prevButton = document.getElementById("carouselPrevButton");
+const nextButton = document.getElementById("carouselNextButton");
+
+const slideWidth = carouselItems[0].offsetWidth;
+
+const numSlides = carouselItems.length;
+
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+    if (direction === "next") {
+        currentIndex++;
+        if (currentIndex === numSlides) {
+            currentIndex = 0; 
+        }
+    } else if (direction === "prev") {
+        currentIndex--;
+        if (currentIndex === -1) {
+            currentIndex = numSlides - 1; 
+        }
+    }
+
+    carouselContainer.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+}
 
 prevButton.addEventListener("click", () => {
-  currentPosition = (currentPosition - 1 + totalContainers) % totalContainers;
-  updateCarousel();
+    moveCarousel("prev");
 });
 
 nextButton.addEventListener("click", () => {
-  currentPosition = (currentPosition + 1) % totalContainers;
-  updateCarousel();
+    moveCarousel("next");
 });
 
-function updateCarousel() {
-  caroselContainers.forEach((container, index) => {
-    const newIndex = (index + currentPosition) % totalContainers;
-    if (index >= currentPosition && index < currentPosition + visibleContainers) {
-      container.style.display = "block";
-    } else {
-      container.style.display = "none";
-    }
-  });
 
-  // Застосовуємо анімацію до всієї каруселі
-  carousel.style.transition = "opacity 0.5s ease-in-out, transform 0.5s ease-in-out";
-  carousel.style.opacity = 0;
-  carousel.style.transform = "translateY(100%)"; // Змінюємо положення на 100% від висоти каруселі
-  setTimeout(() => {
-    carousel.style.opacity = 1;
-    carousel.style.transform = "translateY(0)";
-  }, 0); // Встановлюємо анімацію після короткого таймауту
-}
-
-updateCarousel();
 
 
 
