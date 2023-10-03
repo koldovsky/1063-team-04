1429705287db1b065ad364cc5c46369a25570187
SvyatoslavDuma <45374718+SvyatoslavDuma@users.noreply.github.com>
2023-10-03 20:40:56 +0000

    index.testimonials.css update

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,13 +1,14 @@
 const carousel = document.querySelector(".testimonials-carousel");
-const slides = carousel.querySelectorAll(".carousel__slide");
+const slides = Array.from(carousel.querySelectorAll(".carousel__slide"));
 const prevButton = carousel.querySelector(".carousel__prev");
 const nextButton = carousel.querySelector(".carousel__next");
 
 let currentIndex = 0;
+const slidesToShow = 3;
 
-function showSlide(index) {
+function showSlides() {
   slides.forEach((slide, i) => {
-    if (i === index) {
+    if (i >= currentIndex && i < currentIndex + slidesToShow) {
       slide.style.display = "block";
     } else {
       slide.style.display = "none";
@@ -16,22 +17,24 @@ function showSlide(index) {
 }
 
 function nextSlide() {
-  currentIndex++;
+  currentIndex += slidesToShow;
   if (currentIndex >= slides.length) {
     currentIndex = 0;
+  } else if (currentIndex + slidesToShow > slides.length) {
+    currentIndex = slides.length - slidesToShow;
   }
-  showSlide(currentIndex);
+  showSlides();
 }
 
 function prevSlide() {
-  currentIndex--;
+  currentIndex -= slidesToShow;
   if (currentIndex < 0) {
-    currentIndex = slides.length - 1;
+    currentIndex = slides.length - slidesToShow;
   }
-  showSlide(currentIndex);
+  showSlides();
 }
 
 prevButton.addEventListener("click", prevSlide);
 nextButton.addEventListener("click", nextSlide);
 
-showSlide(currentIndex);
+showSlides();
