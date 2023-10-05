a34bfa74dd6860948c2d9da3235177e4251a6fad
tohaddid <54992797+VolgarDiablo@users.noreply.github.com>
2023-10-05 10:20:20 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 document.addEventListener("partialsLoaded", async () => {
   await import("./trending-header-text.js");
   await import("./scroll-up.js");
-  await import("./shopping-cart.js");
   await import("./recommend-list.js");
+  await import("./shopping-cart.js");
   await import("./testimonials.js");
   await import("./new-arrivals-carousel-cards.js");
   await import("./new-arrivals.js");
+  await import("./banners.and.carousel.js");
   await import("./trending-products-carousel.js");
   await import("./about-us.js");
   await import("./carousel-any-products.js");
